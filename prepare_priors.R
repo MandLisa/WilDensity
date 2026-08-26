ca4c4cb81cb06a8d8f9719fb71eb60900ce8c455
MandLisa <lisa.mandl@tum.de>
2026-08-26 07:51:43 +0000

    update

--- prepare_priors.R
+++ prepare_priors.R
@@ -8,31 +8,40 @@ corine_shp <- "/mnt/eo/WilDensity/_data/_shp/corine/CORINE_2018.shp"
 study_shp  <- "/mnt/eo/WilDensity/_data/_shp/corine/CORINE_2018.shp"
 dem_vrt    <- "/mnt/dss_europe/misc/dem_glo30/glo30_dem.vrt"
 
-out_corine <- "/mnt/eo/WilDensity/_data/_corine"
-out_priors <- "/mnt/eo/WilDensity/_data/_priors"
+out_dir <- "/mnt/eo/WilDensity/_data/_priors"
 
-dir.create(out_corine, recursive = TRUE, showWarnings = FALSE)
-dir.create(out_priors, recursive = TRUE, showWarnings = FALSE)
-
-target_res  <- 100
+target_res   <- 500
 forest_codes <- c(311, 312, 313)
+
+dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
 
 
 # =====================================================================
-# CORINE
+# INPUT DATA
 # =====================================================================
 
 corine <- vect(corine_shp)
+study_area <- vect(study_shp)
+
 corine$Code_18 <- as.integer(corine$Code_18)
+
+if (!same.crs(study_area, corine)) {
+  study_area <- project(study_area, crs(corine))
+}
 
 corine_forest <- corine[
   corine$Code_18 %in% forest_codes,
 ]
 
-r_100m <- rast(
-  ext(corine_forest),
+
+# =====================================================================
+# 500 M TEMPLATE
+# =====================================================================
+
+r_500m <- rast(
+  ext(study_area),
   resolution = target_res,
-  crs = crs(corine_forest)
+  crs = crs(study_area)
 )
 
 
@@ -45,7 +54,7 @@ class_cover <- lapply(
   function(code) {
     rasterize(
       corine_forest[corine_forest$Code_18 == code, ],
-      r_100m,
+      r_500m,
       cover = TRUE,
       background = 0
     )
@@ -54,22 +63,27 @@ class_cover <- lapply(
 
 class_cover <- rast(class_cover)
 
-forest_type_100m <- app(
+forest_type_500m <- app(
   class_cover,
   function(x) {
     if (all(is.na(x) | x == 0)) {
       return(NA)
     }
-    
     forest_codes[which.max(x)]
   }
 )
 
-names(forest_type_100m) <- "forest_type"
+forest_type_500m <- mask(
+  forest_type_500m,
+  study_area,
+  touches = TRUE
+)
+
+names(forest_type_500m) <- "forest_type"
 
 writeRaster(
-  forest_type_100m,
-  file.path(out_corine, "CORINE_forest_types.tif"),
+  forest_type_500m,
+  file.path(out_dir, "CORINE_forest_types_500m.tif"),
   datatype = "INT2U",
   overwrite = TRUE
 )
@@ -79,18 +93,24 @@ writeRaster(
 # PRIOR 2: FOREST COVER
 # =====================================================================
 
-forest_cover_100m <- rasterize(
+forest_cover_500m <- rasterize(
   corine_forest,
-  r_100m,
+  r_500m,
   cover = TRUE,
   background = 0
 )
 
-names(forest_cover_100m) <- "forest_cover"
+forest_cover_500m <- mask(
+  forest_cover_500m,
+  study_area,
+  touches = TRUE
+)
+
+names(forest_cover_500m) <- "forest_cover"
 
 writeRaster(
-  forest_cover_100m,
-  file.path(out_corine, "CORINE_forest_cover.tif"),
+  forest_cover_500m,
+  file.path(out_dir, "CORINE_forest_cover_500m.tif"),
   datatype = "FLT4S",
   overwrite = TRUE
 )
@@ -102,8 +122,8 @@ writeRaster(
 
 dem <- rast(dem_vrt)
 
-bbox <- as.polygons(ext(r_100m))
-crs(bbox) <- crs(r_100m)
+bbox <- as.polygons(ext(r_500m))
+crs(bbox) <- crs(r_500m)
 
 bbox_dem <- project(
   bbox,
@@ -116,100 +136,98 @@ dem_crop <- crop(
   snap = "out"
 )
 
-dem_100m <- project(
+dem_500m <- project(
   dem_crop,
-  r_100m,
+  r_500m,
   method = "bilinear"
 )
 
 
 # =====================================================================
-# PRIOR 3: SLOPE
+# PRIOR 3: ELEVATION
 # =====================================================================
 
-slope_100m <- terrain(
-  dem_100m,
+elevation_500m <- mask(
+  dem_500m,
+  study_area,
+  touches = TRUE
+)
+
+names(elevation_500m) <- "elevation"
+
+writeRaster(
+  elevation_500m,
+  file.path(out_dir, "elevation_500m.tif"),
+  datatype = "FLT4S",
+  overwrite = TRUE
+)
+
+
+# =====================================================================
+# PRIOR 4: SLOPE
+# =====================================================================
+
+slope_500m <- terrain(
+  dem_500m,
   v = "slope",
   unit = "degrees",
   neighbors = 8
 )
 
-names(slope_100m) <- "slope"
+slope_500m <- mask(
+  slope_500m,
+  study_area,
+  touches = TRUE
+)
+
+names(slope_500m) <- "slope"
+
+writeRaster(
+  slope_500m,
+  file.path(out_dir, "slope_500m.tif"),
+  datatype = "FLT4S",
+  overwrite = TRUE
+)
 
 
 # =====================================================================
-# PRIOR 4: RUGGEDNESS
+# PRIOR 5: RUGGEDNESS
 # =====================================================================
 
-ruggedness_100m <- terrain(
-  dem_100m,
+ruggedness_500m <- terrain(
+  dem_500m,
   v = "TRI",
   neighbors = 8
 )
 
-names(ruggedness_100m) <- "ruggedness"
-
-
-# =====================================================================
-# STUDY AREA
-# =====================================================================
-
-study_area <- vect(study_shp)
-
-if (!same.crs(study_area, r_100m)) {
-  study_area <- project(
-    study_area,
-    crs(r_100m)
-  )
-}
-
-elevation_study <- crop(
-  dem_100m,
+ruggedness_500m <- mask(
+  ruggedness_500m,
   study_area,
-  mask = TRUE,
   touches = TRUE
 )
 
-slope_study <- crop(
-  slope_100m,
-  study_area,
-  mask = TRUE,
-  touches = TRUE
-)
-
-ruggedness_study <- crop(
-  ruggedness_100m,
-  study_area,
-  mask = TRUE,
-  touches = TRUE
-)
-
-names(elevation_study)  <- "elevation"
-names(slope_study)      <- "slope"
-names(ruggedness_study) <- "ruggedness"
-
-
-# =====================================================================
-# SAVE TERRAIN PRIORS
-# =====================================================================
+names(ruggedness_500m) <- "ruggedness"
 
 writeRaster(
-  elevation_study,
-  file.path(out_priors, "elevation_100m.tif"),
+  ruggedness_500m,
+  file.path(out_dir, "ruggedness_500m.tif"),
   datatype = "FLT4S",
   overwrite = TRUE
 )
 
-writeRaster(
-  slope_study,
-  file.path(out_priors, "slope_100m.tif"),
-  datatype = "FLT4S",
-  overwrite = TRUE
+
+# =====================================================================
+# CHECK
+# =====================================================================
+
+priors <- c(
+  forest_type_500m,
+  forest_cover_500m,
+  elevation_500m,
+  slope_500m,
+  ruggedness_500m
 )
 
-writeRaster(
-  ruggedness_study,
-  file.path(out_priors, "ruggedness_100m.tif"),
-  datatype = "FLT4S",
-  overwrite = TRUE
-)
+res(priors)
+ext(priors)
+crs(priors)
